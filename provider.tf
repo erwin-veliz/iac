e9f4db1356ab73fe5b13b2d10f01ec352dcc69c4
Erwin <evelizgr@fibernaas.onmicrosoft.com>
2025-07-10 16:57:10 -0500

    add apply

--- provider.tf
+++ provider.tf
@@ -4,12 +4,12 @@ terraform {
     resource_group_name   = "tfstate-rg" ###### crear previamente
     storage_account_name  = "tfstatestorage0101" ###### crear previamente
     container_name        = "tfstate"
-    key                   = "aprod-iaac-ibk.tfstate"
+    key                   = "aprod-iaac.tfstate"
   }  
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "3eb4fcf9-7b07-492e-a136-85e534bc7696"
+  # subscription_id = "3eb4fcf9-7b07-492e-a136-85e534bc7696"
 }
