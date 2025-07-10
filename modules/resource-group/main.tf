0b12076b96d5d3a3adfc58d1bba83e2c31994581
Erwin <evelizgr@fibernaas.onmicrosoft.com>
2025-07-10 15:58:22 -0500

    update names

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -1,6 +1,6 @@
 module "naming_convention" {
-  #source = "git::https://dev.azure.com/FET-Hispam-TI/Terraform-Iaac/_git/terraform-iaac-naming_convention"
-  source = "../terraform-iaac-naming_convention"
+  #source = "git::https://dev.azure.com/FET-Hispam-TI/Terraform-Iaac/_git/naming_convention"
+  source = "../naming_convention"
   resource_code = "rg"
   purpose_abreviation = var.purpose_abreviation
   global-abreviations = var.global-abreviations
@@ -8,8 +8,8 @@ module "naming_convention" {
 }
 
 module "tagging_convention" {
-  #source = "git::https://dev.azure.com/FET-Hispam-TI/Terraform-Iaac/_git/terraform-iaac-tagging_convention"
-  source = "../terraform-iaac-tagging_convention"
+  #source = "git::https://dev.azure.com/FET-Hispam-TI/Terraform-Iaac/_git/tagging_convention"
+  source = "../tagging_convention"
   global-tags = var.global-tags
   purpose_description = var.purpose_description
 }
